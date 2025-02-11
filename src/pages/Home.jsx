2cb10febaae4b1fa172bd2ad3c8637fa259e4577
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-02-11 19:29:32 +0500

    add header,how it works,why choice us and popular deals

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Header from '../components/home/Header'
+import Working from '../components/home/Working'
+import WhyUs from '../components/home/WhyUs'
+import PopularServices from '../components/home/PopularServices'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <Header />
+        <Working />
+        <WhyUs />
+        <PopularServices />
+    </div>
   )
 }
 
