1981c5e2b62caaf2212d0c070227b9f46c84283c
HamzaMajeed86 <rahamza146@gmail.com>
2025-03-13 11:10:01 -0700

    done blog detail page

--- frontend/src/components/blogs/BlogCards.jsx
+++ frontend/src/components/blogs/BlogCards.jsx
@@ -1,116 +1,32 @@
 import React, { useState } from "react";
-
-const blogs = [
-  {
-    id: 1,
-    title: "The Future of AI: Trends to Watch",
-    image:
-      "https://plus.unsplash.com/premium_photo-1682434403587-1313db01ed02?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmxvZ3N8ZW58MHx8MHx8fDA%3D",
-    description:
-      "Discover the latest trends in AI that are shaping our future, from deep learning to AI ethics.",
-    category: "AI",
-  },
-  {
-    id: 2,
-    title: "10 Must-Have Tech Gadgets in 2025",
-    image:
-      "https://plus.unsplash.com/premium_photo-1661265944044-bc7248ae54f9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmxvZ3N8ZW58MHx8MHx8fDA%3D",
-    description:
-      "A roundup of the coolest and most innovative tech gadgets expected to dominate in 2025.",
-    category: "Tech",
-  },
-  {
-    id: 3,
-    title: "The Role of Biotechnology in Medicine",
-    image:
-      "https://images.unsplash.com/photo-1526566762798-8fac9c07aa98?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YmxvZ3N8ZW58MHx8MHx8fDA%3D",
-    description:
-      "Learn how biotech innovations are transforming the healthcare industry and patient treatments.",
-    category: "Bio",
-  },
-  {
-    id: 4,
-    title: "Exploring the Universe: NASA’s Latest Missions",
-    image:
-      "https://images.unsplash.com/photo-1512486130939-2c4f79935e4f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8YmxvZ3N8ZW58MHx8MHx8fDA%3D",
-    description:
-      "NASA is embarking on new space explorations! Here's what you need to know about upcoming missions.",
-    category: "Science",
-  },
-  {
-    id: 5,
-    title: "How to Stay Healthy in a Digital World",
-    image:
-      "https://images.unsplash.com/photo-1457369804613-52c61a468e7d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2dzfGVufDB8fDB8fHww",
-    description:
-      "With screen time increasing, here are some expert tips to keep your mind and body healthy.",
-    category: "Health",
-  },
-  {
-    id: 6,
-    title: "Top 10 Breathtaking Travel Destinations",
-    image:
-      "https://plus.unsplash.com/premium_photo-1681755915233-9acafb348a7f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGJsb2dzfGVufDB8fDB8fHww",
-    description:
-      "From the Swiss Alps to tropical beaches, explore the best destinations for your next vacation.",
-    category: "Travel",
-  },
-  {
-    id: 7,
-    title: "The Future of Education: Digital Learning",
-    image:
-      "https://images.unsplash.com/photo-1500989145603-8e7ef71d639e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGJsb2dzfGVufDB8fDB8fHww",
-    description:
-      "How online learning platforms and AI tutors are changing the way students learn.",
-    category: "Education",
-  },
-  {
-    id: 8,
-    title: "How to Build a Successful Business in 2025",
-    image:
-      "https://images.unsplash.com/photo-1473186505569-9c61870c11f9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGJsb2dzfGVufDB8fDB8fHww",
-    description:
-      "Entrepreneurship tips and strategies for launching a profitable business in today's digital age.",
-    category: "Business",
-  },
-  {
-    id: 9,
-    title: "Sports Analytics: The Game-Changer for Athletes",
-    image:
-      "https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJsb2dzfGVufDB8fDB8fHww",
-    description:
-      "From AI-driven performance tracking to smart wearables, sports analytics is transforming the industry.",
-    category: "Sports",
-  },
-  {
-    id: 10,
-    title: "Personal Finance Tips for Young Professionals",
-    image:
-      "https://images.unsplash.com/photo-1452421822248-d4c2b47f0c81?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGJsb2dzfGVufDB8fDB8fHww",
-    description:
-      "A guide to saving, investing, and managing finances for young professionals.",
-    category: "Finance",
-  },
-];
+import { useNavigate } from "react-router-dom";
+import blogs from "./blogData";
 
 function BlogCards() {
   const [blogPosts, setBlogPosts] = useState(blogs);
+  const navigate = useNavigate();
 
   return (
     <div className="p-4">
-      <div className="grid md:grid-cols-2 gap-6">
+      <div className="grid md:grid-cols-2 gap-6 hover:cursor-pointer">
         {blogPosts.map((item) => (
-          <div key={item.id} className="flex flex-col gap-2">
+          <div
+            key={item.id}
+            className="flex flex-col gap-2"
+            onClick={() => navigate(`/blog/${item.id}`)}
+          >
             <img
               src={item.image}
               alt="not found"
               className="rounded-xl h-[40vh]"
             />
-            <p className="bg-[#EBF1F8] py-1 px-2 rounded-full text-[#726CB0] font-semibold hover:cursor-pointer w-20 text-center text-sm">
+            <p className="bg-[#EBF1F8] py-1 px-2 rounded-full text-[#1447e6] font-semibold hover:cursor-pointer w-20 text-center text-sm">
               {item.category}
             </p>
             <h2 className="lg:text-[1.2rem] md:text-[1rem] text-[20px] font-bold text-[#333]">
-              {item.title}
+              {item.title.length > 50
+                ? `${item.title.substring(0, 50)}...`
+                : item.title}
             </h2>
             <p className="text-[#666]">
               {item.description.length > 100
